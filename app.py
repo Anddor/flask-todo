71e05dbe457d3ba27d568e471f46c1e0779b884b
Andreas Dørum <andreas.doerum@remarkable.no>
2023-01-25 13:39:10 +0100

    run linter

--- app.py
+++ app.py
@@ -12,19 +12,22 @@ app = Flask(__name__)
 
 DATABASE_FILE = 'database.db'
 
+
 @app.route('/')
 def _home():
     """Serve index.html at the root url"""
     return send_from_directory(static, 'index.html')
+
 
 @app.route('/<path:path>')
 def _static(path):
     """Serve content from the static directory"""
     return send_from_directory(static, path)
 
+
 api = Api(app, version='1.0', title='TodoMVC API',
-    description='A simple TodoMVC API', doc='/documentation'
-)
+          description='A simple TodoMVC API', doc='/documentation'
+          )
 
 ns = api.namespace('todos', description='TODO operations', path='/api/todos')
 
@@ -35,26 +38,27 @@ todo = api.model('Todo', {
 })
 
 
-class TodoDAO(object):    
+class TodoDAO(object):
     def __init__(self):
         self.setup_database()
 
     """
     Kjører SQL-spørringen 'statement' med gitte verdi-bindinger 'values'
     """
+
     def _execute_sql(self, statement, values):
-        with contextlib.closing(sqlite3.connect(DATABASE_FILE)) as conn: # auto-closes
-            with conn: # auto-commits
-                with contextlib.closing(conn.cursor()) as cursor: # auto-closes
+        with contextlib.closing(sqlite3.connect(DATABASE_FILE)) as conn:  # auto-closes
+            with conn:  # auto-commits
+                with contextlib.closing(conn.cursor()) as cursor:  # auto-closes
                     cursor.execute(statement, values)
                     fetch = cursor.fetchall()
                     lastrow = cursor.lastrowid
-                    return fetch, lastrow         
+                    return fetch, lastrow
 
-    
     """
     Kjører SQL-spørringen om returnere alt som matcher, bruk til SELECT.
     """
+
     def _execute_sql_fetchall(self, statement, values):
         fetch, _ = self._execute_sql(statement, values)
         return fetch
@@ -62,39 +66,40 @@ class TodoDAO(object):
     """
     Kjører SQL-spørringen og returnerer iden til siste rad. Bruk til INSERT.
     """
+
     def _execute_sql_lastrowid(self, statement, values):
         _, lastrow = self._execute_sql(statement, values)
         return lastrow
 
-
     def _map_todo(self, todo_row):
         return {
-                'id': todo_row[0],
-                'task': todo_row[1],
-                'fav': todo_row[2]
-            }
+            'id': todo_row[0],
+            'task': todo_row[1],
+            'fav': todo_row[2]
+        }
 
     """
     Opprett tabeller i databasen om database-filen ikke finnes fra før.
     """
+
     def setup_database(self):
-        if not os.exists(DATABASE_FILE):
+        if not os._exists(DATABASE_FILE):  # python compatiblity
             # OPPGAVE: Skriv SQL som oppretter en tabell med feltene i en todo:
 
-                self._execute_sql('''CREATE TABLE ...
+            self._execute_sql('''CREATE TABLE ...
             )''', {})
-    
+
     def get_all(self):
         return_list = []
         # OPPGAVE: Skriv SQL som hentesr alle radene i todo tabellen
 
         todos = self._execute_sql_fetchall('''
         SELECT ...
-        ''', {}) # todo convert to json? marshal? ORM?
+        ''', {})  # todo convert to json? marshal? ORM?
         for todo in todos:
             return_list.append(self.map_todo(todo))
         return return_list
-    
+
     def get(self, id):
 
         # OPPGAVE: Skriv SQL som henter todo-raden med den gitte id-en
@@ -125,7 +130,9 @@ class TodoDAO(object):
         DELETE ...
         ''', {'id': id})
 
+
 DAO = TodoDAO()
+
 
 @ns.route('/')
 class TodoList(Resource):
